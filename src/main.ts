4e62662294b7a5ccaaca4db9850c91400990c70d
Hilaladiii <abdulhilal242@gmail.com>
2026-01-06 09:39:08 +0700

    chore: config versioning & cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 import * as dotenv from 'dotenv';
 import { ResponseInterceptor } from './commons/providers/interceptor/response/response.interceptor';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { HttpExceptionFilter } from './commons/filters/http-exception.filter';
 import { setupSwagger } from './lib/swagger';
 import * as cookieParser from 'cookie-parser';
@@ -15,13 +15,25 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
-  app.use(cookieParser());
 
   const reflector = app.get(Reflector);
 
   app.useGlobalInterceptors(new ResponseInterceptor(reflector));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
+  app.use(cookieParser());
+
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
 
   setupSwagger(app);
 
